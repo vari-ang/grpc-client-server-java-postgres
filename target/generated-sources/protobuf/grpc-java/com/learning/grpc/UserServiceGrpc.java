dfcae78bf0112c9a4deaced4d372f6f225447a7e
Varianto Angga Prawira <varianto.prawira@gopay.co.id>
2022-10-10 15:47:06 +0700

    chore: update dependency to use postgres & update proto generated files

---
+++ target/generated-sources/protobuf/grpc-java/com/learning/grpc/UserServiceGrpc.java
@@ -0,0 +1,453 @@
+package com.learning.grpc;
+
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.4.0)",
+    comments = "Source: UserService.proto")
+public final class UserServiceGrpc {
+
+  private UserServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "com.learning.grpc.UserService";
+
+  // Static method descriptors that strictly reflect the proto.
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.google.protobuf.Empty,
+      com.learning.grpc.UsersResponse> METHOD_GET_ALL_USERS =
+      io.grpc.MethodDescriptor.<com.google.protobuf.Empty, com.learning.grpc.UsersResponse>newBuilder()
+          .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+          .setFullMethodName(generateFullMethodName(
+              "com.learning.grpc.UserService", "getAllUsers"))
+          .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.google.protobuf.Empty.getDefaultInstance()))
+          .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.UsersResponse.getDefaultInstance()))
+          .build();
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.learning.grpc.UserRequest,
+      com.learning.grpc.User> METHOD_GET_USER =
+      io.grpc.MethodDescriptor.<com.learning.grpc.UserRequest, com.learning.grpc.User>newBuilder()
+          .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+          .setFullMethodName(generateFullMethodName(
+              "com.learning.grpc.UserService", "getUser"))
+          .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.UserRequest.getDefaultInstance()))
+          .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.User.getDefaultInstance()))
+          .build();
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.learning.grpc.User,
+      com.learning.grpc.GeneralResponse> METHOD_CREATE =
+      io.grpc.MethodDescriptor.<com.learning.grpc.User, com.learning.grpc.GeneralResponse>newBuilder()
+          .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+          .setFullMethodName(generateFullMethodName(
+              "com.learning.grpc.UserService", "create"))
+          .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.User.getDefaultInstance()))
+          .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.GeneralResponse.getDefaultInstance()))
+          .build();
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.learning.grpc.User,
+      com.learning.grpc.GeneralResponse> METHOD_UPDATE =
+      io.grpc.MethodDescriptor.<com.learning.grpc.User, com.learning.grpc.GeneralResponse>newBuilder()
+          .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+          .setFullMethodName(generateFullMethodName(
+              "com.learning.grpc.UserService", "update"))
+          .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.User.getDefaultInstance()))
+          .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.GeneralResponse.getDefaultInstance()))
+          .build();
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.learning.grpc.UserRequest,
+      com.learning.grpc.GeneralResponse> METHOD_DELETE =
+      io.grpc.MethodDescriptor.<com.learning.grpc.UserRequest, com.learning.grpc.GeneralResponse>newBuilder()
+          .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+          .setFullMethodName(generateFullMethodName(
+              "com.learning.grpc.UserService", "delete"))
+          .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.UserRequest.getDefaultInstance()))
+          .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+              com.learning.grpc.GeneralResponse.getDefaultInstance()))
+          .build();
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static UserServiceStub newStub(io.grpc.Channel channel) {
+    return new UserServiceStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static UserServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new UserServiceBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static UserServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new UserServiceFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class UserServiceImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public void getAllUsers(com.google.protobuf.Empty request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.UsersResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_GET_ALL_USERS, responseObserver);
+    }
+
+    /**
+     */
+    public void getUser(com.learning.grpc.UserRequest request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.User> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_GET_USER, responseObserver);
+    }
+
+    /**
+     */
+    public void create(com.learning.grpc.User request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_CREATE, responseObserver);
+    }
+
+    /**
+     */
+    public void update(com.learning.grpc.User request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_UPDATE, responseObserver);
+    }
+
+    /**
+     */
+    public void delete(com.learning.grpc.UserRequest request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_DELETE, responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            METHOD_GET_ALL_USERS,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.google.protobuf.Empty,
+                com.learning.grpc.UsersResponse>(
+                  this, METHODID_GET_ALL_USERS)))
+          .addMethod(
+            METHOD_GET_USER,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.learning.grpc.UserRequest,
+                com.learning.grpc.User>(
+                  this, METHODID_GET_USER)))
+          .addMethod(
+            METHOD_CREATE,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.learning.grpc.User,
+                com.learning.grpc.GeneralResponse>(
+                  this, METHODID_CREATE)))
+          .addMethod(
+            METHOD_UPDATE,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.learning.grpc.User,
+                com.learning.grpc.GeneralResponse>(
+                  this, METHODID_UPDATE)))
+          .addMethod(
+            METHOD_DELETE,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.learning.grpc.UserRequest,
+                com.learning.grpc.GeneralResponse>(
+                  this, METHODID_DELETE)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class UserServiceStub extends io.grpc.stub.AbstractStub<UserServiceStub> {
+    private UserServiceStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private UserServiceStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected UserServiceStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new UserServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void getAllUsers(com.google.protobuf.Empty request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.UsersResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_GET_ALL_USERS, getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void getUser(com.learning.grpc.UserRequest request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.User> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_GET_USER, getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void create(com.learning.grpc.User request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_CREATE, getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void update(com.learning.grpc.User request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_UPDATE, getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void delete(com.learning.grpc.UserRequest request,
+        io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_DELETE, getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class UserServiceBlockingStub extends io.grpc.stub.AbstractStub<UserServiceBlockingStub> {
+    private UserServiceBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private UserServiceBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected UserServiceBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new UserServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.learning.grpc.UsersResponse getAllUsers(com.google.protobuf.Empty request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_GET_ALL_USERS, getCallOptions(), request);
+    }
+
+    /**
+     */
+    public com.learning.grpc.User getUser(com.learning.grpc.UserRequest request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_GET_USER, getCallOptions(), request);
+    }
+
+    /**
+     */
+    public com.learning.grpc.GeneralResponse create(com.learning.grpc.User request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_CREATE, getCallOptions(), request);
+    }
+
+    /**
+     */
+    public com.learning.grpc.GeneralResponse update(com.learning.grpc.User request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_UPDATE, getCallOptions(), request);
+    }
+
+    /**
+     */
+    public com.learning.grpc.GeneralResponse delete(com.learning.grpc.UserRequest request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_DELETE, getCallOptions(), request);
+    }
+  }
+
+  /**
+   */
+  public static final class UserServiceFutureStub extends io.grpc.stub.AbstractStub<UserServiceFutureStub> {
+    private UserServiceFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private UserServiceFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected UserServiceFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new UserServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.learning.grpc.UsersResponse> getAllUsers(
+        com.google.protobuf.Empty request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_GET_ALL_USERS, getCallOptions()), request);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.learning.grpc.User> getUser(
+        com.learning.grpc.UserRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_GET_USER, getCallOptions()), request);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.learning.grpc.GeneralResponse> create(
+        com.learning.grpc.User request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_CREATE, getCallOptions()), request);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.learning.grpc.GeneralResponse> update(
+        com.learning.grpc.User request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_UPDATE, getCallOptions()), request);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.learning.grpc.GeneralResponse> delete(
+        com.learning.grpc.UserRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_DELETE, getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_GET_ALL_USERS = 0;
+  private static final int METHODID_GET_USER = 1;
+  private static final int METHODID_CREATE = 2;
+  private static final int METHODID_UPDATE = 3;
+  private static final int METHODID_DELETE = 4;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final UserServiceImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(UserServiceImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_GET_ALL_USERS:
+          serviceImpl.getAllUsers((com.google.protobuf.Empty) request,
+              (io.grpc.stub.StreamObserver<com.learning.grpc.UsersResponse>) responseObserver);
+          break;
+        case METHODID_GET_USER:
+          serviceImpl.getUser((com.learning.grpc.UserRequest) request,
+              (io.grpc.stub.StreamObserver<com.learning.grpc.User>) responseObserver);
+          break;
+        case METHODID_CREATE:
+          serviceImpl.create((com.learning.grpc.User) request,
+              (io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse>) responseObserver);
+          break;
+        case METHODID_UPDATE:
+          serviceImpl.update((com.learning.grpc.User) request,
+              (io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse>) responseObserver);
+          break;
+        case METHODID_DELETE:
+          serviceImpl.delete((com.learning.grpc.UserRequest) request,
+              (io.grpc.stub.StreamObserver<com.learning.grpc.GeneralResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static final class UserServiceDescriptorSupplier implements io.grpc.protobuf.ProtoFileDescriptorSupplier {
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return com.learning.grpc.UserServiceOuterClass.getDescriptor();
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (UserServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new UserServiceDescriptorSupplier())
+              .addMethod(METHOD_GET_ALL_USERS)
+              .addMethod(METHOD_GET_USER)
+              .addMethod(METHOD_CREATE)
+              .addMethod(METHOD_UPDATE)
+              .addMethod(METHOD_DELETE)
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
